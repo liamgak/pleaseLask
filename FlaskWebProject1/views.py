ca8cd74e87e0dafea7cb03a465532546bb7e22b0
liamgak <39654900+liamgak@users.noreply.github.com>
2018-09-18 16:28:39 +0900

    Update views.py

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -7,31 +7,9 @@ from flask import render_template
 from FlaskWebProject1 import app
 
 @app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+def index():
+    return render_template('index.html')
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+@app.route('/hci')
+def hci():
+    return render_template('HCI.html')
